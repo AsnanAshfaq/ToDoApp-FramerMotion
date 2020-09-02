b99fcf67244c4a795d8ed94e53763ab5269da836
Asnan Ashfaq <65377376+AsnanAshfaq@users.noreply.github.com>
2020-09-03 03:32:00 +0500

    added some framer motion animations

--- src/App.js
+++ src/App.js
@@ -3,51 +3,47 @@ import { motion } from "framer-motion";
 import "./App.css";
 import ListItem from "./ListItem";
 import ReactLoading from "react-loading";
-import * as firebase from 'firebase'
-
+import * as firebase from "firebase";
 
 function App() {
-
   // some local states 💀☢️
   const [Input, setInput] = useState("");
   const [IncomingData, setIncomingData] = useState({});
   const [isLoading, setisLoading] = useState(true);
 
-
   //delte function to delete the todos ❎
   const deleteData = (id) => {
-    const rootRef = firebase.database().ref('todoapp-framermotion').child(id)
-    rootRef.remove()
-    setisLoading(true)
-  }
-
+    const rootRef = firebase.database().ref("todoapp-framermotion").child(id);
+    rootRef.remove();
+    setisLoading(true);
+  };
 
   // add function to add todos to the firesbase ➕
   const addData = (event) => {
     event.preventDefault();
-    setisLoading(true)
-    // add value to the database 
-    const rootRef = firebase.database().ref('todoapp-framermotion')
-    rootRef.push(Input)
-    // set incoming variable to the response we get from firebase
-    rootRef.on('value', (snapShot) => {
-      setIncomingData(snapShot.val())
-      setisLoading(false)
-    })
+    setisLoading(true);
+    // add value to the database
+    const rootRef = firebase.database().ref("todoapp-framermotion");
+    rootRef.push(Input);
+    // set incoming variable to the response we get from firebase 👽
+    rootRef.on("value", (snapShot) => {
+      setIncomingData(snapShot.val());
+      setisLoading(false);
+    });
     setInput("");
   };
 
   useEffect(() => {
     // make instance of firebase 🥃
-    const rootRef = firebase.database().ref('todoapp-framermotion')
-    rootRef.on('value', (snapShot) => {
-      setIncomingData(snapShot.val())
-      setisLoading(false)
-    })
-    if(Object.keys(IncomingData).length != 0){
-      setisLoading(false)
+    const rootRef = firebase.database().ref("todoapp-framermotion");
+    rootRef.on("value", (snapShot) => {
+      setIncomingData(snapShot.val());
+      setisLoading(false);
+    });
+    if (IncomingData != {}) {
+      setisLoading(false);
     }
-  },[isLoading]);
+  }, [isLoading]);
 
   return (
     <div className="app">
@@ -55,24 +51,35 @@ function App() {
       <div className="container-fluid  d-flex flex-column">
         <div className="row">
           <div className="col d-flex justify-content-center">
-            <h1 className="text-light Pacifico-Font m-2">To Do App </h1>
+            <motion.h1
+              className="text-light Pacifico-Font m-2"
+              initial={{ x: 400, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              style={{userSelect: "none"}}
+              transition={{delay:0.4, type:"spring", stiffness:140}}
+            >
+              To Do App{" "}
+            </motion.h1>
           </div>
         </div>
         {/* input to add todo's 🥇 */}
         <div className="container-fluid mt-3 mx-2">
           <div className="row mx-4 d-flex justify-content-center">
             <div className="col-7 mx-4 p-0">
-              <input
+              <motion.input
                 type="text"
                 className="form-control w-100 Satisfy"
                 style={{ fontSize: 18 }}
                 placeholder="Enter today's task"
                 value={Input}
                 onChange={(event) => setInput(event.target.value)}
+                initial={{  opacity:0 }}
+                animate={{  opacity:1 }}
+                transition={{ duration:2.3,delay:0.3}}
               />
             </div>
             <div className="col-1 m-0 p-0">
-              <button
+              <motion.button
                 className="btn  w-100 Merriweather"
                 style={{
                   fontSize: 18,
@@ -81,18 +88,42 @@ function App() {
                 }}
                 disabled={!Input}
                 onClick={(event) => addData(event)}
+                initial={{  opacity:0 }}
+                animate={{  opacity:1 }}
+                transition={{ duration:2.3,delay:0.3}}
               >
                 Add
-              </button>
+              </motion.button>
             </div>
           </div>
         </div>
         {/* list of todo 🚀 */}
         {isLoading === false ? (
           <div>
-            {Object.entries(IncomingData).map( ([key, todo]) => (
-              <ListItem todo={todo} key={key} id={key} deleteTodo={deleteData}/>
-            ))}
+            {/* if we donot have any data 😞 then show a message 📧 */}
+            {IncomingData == null ? (
+              <div className="d-flex justify-content-center">
+                <motion.p
+                  className="text-light mt-3 Merriweather"
+                  style={{ userSelect: "none", fontSize: 21 }}
+                  initial={{opacity:0}}
+                  animate={{opacity:1}}
+                  transition={{duration:2}}
+                >
+                  Not any Todo's
+                </motion.p>
+              </div>
+            ) : (
+              Object.entries(IncomingData).map(([key, todo]) => (
+                // show todo's list component 💯
+                <ListItem
+                  todo={todo}
+                  key={key}
+                  id={key}
+                  deleteTodo={deleteData}
+                />
+              ))
+            )}
           </div>
         ) : (
           <div className="d-flex justify-content-center">
--- src/ListItem.js
+++ src/ListItem.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./ListItem.css";
+import { motion } from "framer-motion";
 
 function ListItem({ todo, id, deleteTodo }) {
   return (
-    <div className="container-fluid d-flex justify-content-center mt-3">
+    <motion.div
+      className="container-fluid d-flex justify-content-center mt-3"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 1 }}
+    >
       <div
         className="row w-50 d-flex justify-content-center"
         style={{ backgroundColor: "#375051", borderRadius: 10, padding: 10 }}
@@ -11,22 +17,23 @@ function ListItem({ todo, id, deleteTodo }) {
         <p
           className="Merriweather text-light m-0 p-0"
           style={{ userSelect: "none", fontSize: 21 }}
-          
         >
           {todo}
         </p>
         <div className="ml-4 mt-1">
-          <i
+          <motion.i
             className="fa fa-minus"
             id={id}
             style={{ cursor: "pointer" }}
             onClick={(event) => {
-              deleteTodo(event.target.id)
+              deleteTodo(event.target.id);
             }}
-          ></i>
+            whileHover={{ scale: 1.5, originX:0, color:'black' }}
+            transition={{type:"spring",stiffness:499}}
+          ></motion.i>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 
